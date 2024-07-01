aa8717ce91d420cc0292ad79b66a1e09fe375caf
Duy Nguyen Manh <108318095+DuyNM0601@users.noreply.github.com>
2024-07-01 15:35:11 +0700

    add mainifest

--- app/src/main/java/com/example/twovn/MainActivity.java
+++ app/src/main/java/com/example/twovn/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+
+import com.example.profile_pj.ProfileFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import android.view.MenuItem;
 
